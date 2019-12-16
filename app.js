09b163e6a8c38069cea2f4de9436f2c2eeba7618
summerag <alex.summer48@yahoo.com>
2019-12-16 07:07:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-document.getElementById("content").innerHTML
+const play = `
+  <div class="playerChoices">
+        <button class="playerChoice">Rock</button>
+        <button class="playerChoice">Paper</button>
+        <button class="playerChoice">Scissors</button>
+  </div>
+`;
+
+document.getElementById("content").innerHTML = play
