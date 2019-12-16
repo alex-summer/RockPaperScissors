231db8d2a381808c6e02cfa3f88307a939b4fd3f
summerag <alex.summer48@yahoo.com>
2019-12-16 13:49:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const home = `
+    <h2>Number of games?</h2>
+    <form id="gameOptions">
+        <select class="playerChoice" id="selectButton">
+            <option value="1">1</option>
+            <option value="3">3</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+        </select><br>
+        <input class="playerChoice" id="playButton" type="submit" value="play">
+    </form> `;
+
 const play = `
-  <div class="playerChoices">
+    <div class="playerChoices">
         <button class="playerChoice">Rock</button>
         <button class="playerChoice">Paper</button>
         <button class="playerChoice">Scissors</button>
-  </div>
-`;
+    </div>`;
 
-function playGame(){
+let var displayNumber = 0;
+
+function display(){
   document.getElementById("content").innerHTML = play
 }
 
